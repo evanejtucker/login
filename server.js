e63d2018f2fef7ea08fc5deb9c22a319e57abbbe
Evan Tucker <eej.tucker@gmail.com>
2017-12-21 12:09:54 -0700

    :boat:

--- server.js
+++ server.js
@@ -19,11 +19,24 @@ app.use(morgan('tiny'));
 // use static file 'public'
 app.use(express.static('public'));
 
+// main page
 app.get('/', (req, res) => {
     res.sendFile('public/index.html')
 });
 
+// users route
 app.use('/users', users)
+
+
+// connect to mongo
+mongoose.connect('mongodb://localhost/users', { useMongoClient: true });
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("connected to db");
+});
+
 
 // start server
 app.listen(3000, () => {

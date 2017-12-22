519ff58d9ca795288c4d9b261c07aa5bbc76d1b0
Evan Tucker <eej.tucker@gmail.com>
2017-12-21 18:43:13 -0700

    :bee:

--- public/assets/javascript/script.js
+++ public/assets/javascript/script.js
@@ -11,12 +11,21 @@ $( document ).ready(function() {
 // Functions
 // -----------------------------------------------------------------------------------------------------
 
+    // grab user info from modal
+    var addUser = () => {
+        let firstName = $('#first_name').val().trim();
+        let lastName = $('#last_name').val().trim();
+        let userName = $('#newUsername').val().trim();
+        let password = $('#confirmPassword').val().trim();
+        let email = $('#email').val().trim();
 
+        console.log(firstName + " " + lastName + " " + userName + " " + password + " " + email);
+    }
 
 
 // Main Process
 // -----------------------------------------------------------------------------------------------------
 
-
+$('#createNewUser').on('click', addUser);
 
 });

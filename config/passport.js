0e23e86c723e7a89df06f7b5268f86d907fdc0ea
Evan Tucker <eej.tucker@gmail.com>
2017-12-23 16:17:30 -0700

    :sun:

---
+++ config/passport.js
@@ -0,0 +1,9 @@
+
+var passport = require('passport')
+var LocalStrategy = require('passport-local').Strategy;
+var User = require("./models/User.js");
+
+module.exports = function(passport) {
+
+}
+
--- server.js
+++ server.js
@@ -1,64 +1,39 @@
 
 // dependencies
-const morgan = require('morgan');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const session = require('express-session');
 
 // require routers
 var users = require('./routes/users')
 
 // user schema
-mongoose.Promise = Promise;
 var User = require("./models/User.js");
 
-// body-parser
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json())
-
-// use morgan logging middleware
-app.use(morgan('tiny'));
-
-// use static file 'public'
-app.use(express.static('public'));
-
-// main page
-app.get('/', (req, res) => {
-    res.sendFile('public/index.html')
-});
-
-app.post('/submit', (req, res, next) => {
-    let userInfo = req.body;
-    res.status(200).send(userInfo);
-    let newUser = new User({
-        firstname: userInfo.firstname, 
-        lastname: userInfo.lastname, 
-        username: userInfo.username, 
-        password: userInfo.password, 
-        email: userInfo.email
-    });
-    newUser.save(function (err, newUser) {
-        if (err) return console.error(err);
-      });
-    console.log(newUser);
-});
-
-app.get('/findall', (req, res, next)=> {
-    User.find(function(err, user) {
-        if (err) return console.error(err);
-        res.send(user);
-        console.log(user);
-    });
-});
-
-// users route
+// users router
 app.use('/users', users)
+
+    // middleware
+    app.use(express.cookieParser());
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json())
+    app.use(morgan('tiny'));
+    app.use(express.static('public'));
+    app.use(session({ secret: "cats" }));
+    app.use(passport.initialize());
+    app.use(passport.session());
 
 
 // connect to mongo
 mongoose.connect('mongodb://localhost/users', { useMongoClient: true });
-mongoose.Promise = global.Promise;
+// mongoose.Promise = global.Promise;
+// mongoose.Promise = Promise;
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
@@ -66,7 +41,12 @@ db.once('open', function() {
 });
 
 
+// main page
+app.get('/', (req, res) => {
+    res.sendFile('public/index.html')
+});
+
 // start server
-app.listen(3000, () => {
-    console.log('Example app listening on port 3000!')
+app.listen(port, () => {
+    console.log('Example app listening on port ' + port)
 });
